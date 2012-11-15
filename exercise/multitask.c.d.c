f709a18933c73fc5dfad1ac412cd8c15c46ebe1d
LucaREz <luca.rez@gmail.com>
2012-11-15 12:50:14 -0600

    esercizi

--- exercise/multitask.c.d.c
+++ exercise/multitask.c.d.c
@@ -5,11 +5,9 @@
 
 #include <native/task.h>
 #include <native/timer.h>
-#include <native/sem.h>
 
 #include <rtdk.h>
 RT_TASK demo_task;
-RT_SEM s;
 
 void demo (void *arg){
 	sleep(1);
@@ -28,9 +26,7 @@ void demo (void *arg){
 	p*=retval;
 	rt_task_set_periodic(NULL,TM_NOW,p);
 	while(1){
-		rt_sem_p(&s,0);
 		rt_printf("[%s] %d s periodic\n", curtaskinfo.name,retval);
-		rt_sem_v(&s);
 		rt_task_wait_period(NULL);
 	}
 }
@@ -41,20 +37,13 @@ int main (int argc, char * argv[]){
 	int i;
 	rt_print_auto_init(1);
 	mlockall(MCL_CURRENT|MCL_FUTURE);
-	rt_sem_create(&s, "sem", 1, S_PRIO);
 	rt_printf("[Main]:start task\n");
 	for(i=0;i<3;i++){
 		sprintf(str,"Task-%d",i);
 		periods[i]=i+1;
-		rt_task_create(&demo_task,str,10,50+i,0);
+		rt_task_create(&demo_task,str,10,50,0);
 		rt_task_start(&demo_task,&demo,&periods[i]);
 	}
-	//sleep(1);
-	//rt_task_set_periodic(NULL,TM_NOW,5e8);
-	//while(1){
-	//	rt_sem_broadcast(&s);
-	//	rt_task_wait_period(NULL);
-	//}
 	rt_printf("end program by CTRL-C\n");
 	pause();
 }
